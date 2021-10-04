8d7248737cf3c1dccb7ef0dc966b0c5719bb7fb1
phonqha <phongdz254@gmail.com>
2021-10-04 12:46:38 +0700

    8.Styling Up The Cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,51 @@
 import React, { useEffect } from "react";
 //Redux
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+//Componets
+import Game from "../components/Game";
+//Styling and animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
+    //fetch game
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(loadGames());
-    });
+    }, [dispatch]);
+    //get data
+    const { popular, newGames, upcoming } = useSelector((state) => state.games);
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <GameList>
+            <h2>Upcoming Game</h2>
+            <Games>
+                {upcoming.map((game) => (
+                    <Game
+                        name={game.name}
+                        id={game.id}
+                        key={game.id}
+                        released={game.released}
+                        image={game.background_image}
+                    />
+                ))}
+            </Games>
+        </GameList>
     );
 };
+
+const GameList = styled.div`
+    padding: 0rem 5rem;
+    h2 {
+        padding: 5rem 0rem;
+    }
+`;
+const Games = styled.div`
+    min-height: 80vh;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+    grid-column-gap: 3rem;
+    grid-row-gap: 5rem;
+`;
 
 export default Home;
